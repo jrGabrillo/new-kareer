cf086203e1c97a95f51a4b6279ac63a1165ebb98
Rufo Gabrillo <rufo.gabrillo@gmail.com>
2018-01-31 23:22:09 +0800

    Update;

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+var $ = Dom7;
+var app = new Framework7({
+    id: 'rnr.kareer.app',
+    root: '#app',
+    theme: 'md',
+    routes: routes,
+});
+var view = app.views.create('.view-main');
+
+view.router.navigate('/account/');
+// view.router.navigate('/home/');
+// setTimeout(function () {
+//   app.preloader.hide();
+// }, 3000);
---
+++ assets/js/routes.js
@@ -0,0 +1,22 @@
+var routes = [
+    {
+      path: '/home/',
+      url: './pages/home.html',
+    }, 
+    {
+      path: '/signin/',
+      url: './pages/signin.html',
+    },
+    {
+      path: '/signup/',
+      url: './pages/signup.html',
+    },
+    {
+      path: '/account/',
+      url: './pages/account.html',
+    },
+    {
+      path: '(.*)',
+      url: './pages/404.html',
+    },
+];
