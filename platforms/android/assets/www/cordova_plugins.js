55ed9fcff7ebf0f58a41196b3449235fb23c1f2b
Rufo Gabrillo <rufo.gabrillo@gmail.com>
2018-02-07 12:49:52 +0800

    Broken facebook api;

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-facebook5.FacebookConnectPlugin",
+        "file": "plugins/cordova-plugin-facebook5/www/facebook-native.js",
+        "pluginId": "cordova-plugin-facebook5",
+        "clobbers": [
+            "facebookConnectPlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-facebook5": "1.9.1",
+    "cordova-plugin-whitelist": "1.3.3"
+};
+// BOTTOM OF METADATA
+});
--- platforms/browser/www/plugins/cordova-plugin-facebook5/www/facebook-browser.js
+++ platforms/browser/www/plugins/cordova-plugin-facebook5/www/facebook-browser.js
@@ -159,7 +159,7 @@ if (window.location.protocol === "file:") {
 } else {
   window.fbAsyncInit = function() {
     FB.init({
-      appId      : APP_ID,  // APP_ID is populated by the cordova after_prepare hook
+      appId      : 1778426385793075,  // 1778426385793075 is populated by the cordova after_prepare hook
       xfbml      : true,
       version    : 'v2.7'
     });
